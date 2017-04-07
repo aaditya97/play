b9c851560458f967f8d8eaf130cdf3a84ec0e02f
aaditya97 <aaditya.tirodkar@gmail.com>
2017-04-07 10:26:47 +0530

    annotations and pokemon spawn

--- Play/ViewController.swift
+++ Play/ViewController.swift
@@ -9,38 +9,92 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    var pokemon : [Pokemon] = []
 
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapview: MKMapView!
+    
+    @IBAction func yourlocation(_ sender: AnyObject) {
+        let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, 400, 400)
+        self.mapview.setRegion(region, animated: true)
+    
+    }
+    
     var manager = CLLocationManager()
     var update = 0
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Checking the authorization status of the map, required for the app:
         self.manager.delegate = self
+        self.mapview.delegate = self
         if (CLLocationManager.authorizationStatus() == .authorizedWhenInUse){
             
-            self.mapView.showsUserLocation = true
+            self.mapview.showsUserLocation = true
             self.manager.startUpdatingLocation()
+            
+             pokemon = bringAllPokemon()
+            
+            //Authorization status and location setting
+            Timer.scheduledTimer(withTimeInterval: 4, repeats: true, block:{
+                (timer) in
+                if let coordinate = self.manager.location?.coordinate{
+                    let randomPokemon = Int(arc4random_uniform(UInt32(self.pokemon.count)))
+                    let pokemon = self.pokemon[randomPokemon]
+                    
+                    let annotation = PokemonAnnotation(coordinate: coordinate, pokemon: pokemon)
+                    annotation.coordinate = coordinate
+                    annotation.coordinate.latitude += (Double(arc4random_uniform(1000))-500)/300000.0
+                    annotation.coordinate.longitude += (Double(arc4random_uniform(1000))-500)/300000.0
+                    
+                    self.mapview.addAnnotation(annotation)
+
         }
+                }
+                    )
+        }
+            
         else {
             //Requesting authorization when not found:
             self.manager.requestWhenInUseAuthorization()
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+   @objc(mapView:viewForAnnotation:) func mapView(_ mapview: MKMapView, viewFor annotation: MKAnnotation) ->MKAnnotationView?{
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        
+        if annotation is MKUserLocation {
+            annotationView.image = #imageLiteral(resourceName: "player")
+        }else {
+            let pokemon = (annotation as! PokemonAnnotation).pokemon
+            annotationView.image = UIImage(named: pokemon.imageFileName!)
+        }
+         var newFrame = annotationView.frame
+        newFrame.size.width = 40
+        newFrame.size.height = 40
+        annotationView.frame = newFrame
+        
+        return annotationView
+    }
+    
+    
+            func locationManager(_ manager: CLLocationManager , didUpdateLocations: [CLLocation])
+            {
         
         if update < 4{
         let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, 400, 400)
-        self.mapView.setRegion(region, animated: true)
-        }
-        else
-        {
-            update += 1
-        }
-            self.manager.stopUpdatingLocation()
-        }
+        self.mapview.setRegion(region, animated: true)
+           
+                            }
+    
+    
+            else
+            {
+                update += 1
+            }
+        self.manager.stopUpdatingLocation()
+    }
     
 
     override func didReceiveMemoryWarning() {
---
+++ Play/pokedex.swift
@@ -0,0 +1,81 @@
+//
+//  pokedex.swift
+//  Play
+//
+//  Created by admin on 06/04/17.
+//  Copyright © 2017 ACE. All rights reserved.
+//
+
+import UIKit
+
+class pokedex: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableview: UITableView!
+    @IBAction func backtomap(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    var caughtPokemon : [Pokemon] = []
+    var uncaughtPokemon : [Pokemon] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        caughtPokemon = getAllCaughtPokemon()
+        uncaughtPokemon = getAllUnCaughtPokemon()
+        
+        self.tableview.delegate = self
+        self.tableview.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    // to hidden battery bar
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // table view fumctions
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Caught Pokemon"
+        }else {
+            return " uncaught Pokemon"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return caughtPokemon.count
+        }else {
+            return uncaughtPokemon.count
+        }
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableview.dequeueReusableCell(withIdentifier: "reusecell", for: indexPath)
+        var pokemon : Pokemon
+        
+        
+        if indexPath.section == 0 {
+            pokemon = self.caughtPokemon[indexPath.row]
+        }else {
+            pokemon = self.uncaughtPokemon[indexPath.row]
+        }
+        cell.textLabel?.text = pokemon.name
+        cell.imageView?.image = UIImage(named: pokemon.imageFileName!)
+        return cell
+        
+    
+    }
+        }
